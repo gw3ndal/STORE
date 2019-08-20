bfcf50988f4adadf6703edf3e0f4a6f2a02035a7
gw3n <loaye1978@gmail.com>
2019-08-20 21:20:50 +0200

    crea folder config/ + file

---
+++ TEMPLATES/Front_Office_SOUP/mvc/app/config/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+   define("APP_LANG","fr");
+   define("APP_CHARSET","utf-8");
+   define("DB_HOST","localhost");
+   define("DB_NAME","bd_blog_cabinet_0");
+   define("DB_CHARSET","utf8");
+   define("DB_USER","root");
+   define("DB_PASSWORD","");
+   define("POST_TR", 350);
+   define("SITE_NAME", "Le Cabinet de Curiosité");
+   define("PAGE_DESCR_DEFAUT", "Le blog de curiosité féminine");
+   define("DEBUG", true);
+   define("NB_PAR_PAGE", 3);
+   define("DEFAULT_MODULE", "article");
+   define("DEFAULT_ACTION", "index");
