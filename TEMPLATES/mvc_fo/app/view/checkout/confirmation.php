5047860d791dc39d229ae77c552224240d87392a
gw3n <loaye1978@gmail.com>
2019-08-21 17:18:50 +0200

    add include header-&-footer

--- TEMPLATES/mvc_fo/app/view/checkout/confirmation.php
+++ TEMPLATES/mvc_fo/app/view/checkout/confirmation.php
@@ -1,4 +1,5 @@
-	<!-- Content -->
+<!-- HEADER -->
+<?php include("../app/view/layout/header.inc.php");?>	<!-- Content -->
 	<div id="content">
 
 		<!-- Section -->
@@ -14,4 +15,6 @@
 				</div>
 			</div>
 		</section>
+<!-- FOOTER -->
+<?php include("../app/view/layout/footer.inc.php");?>
 
