f4e1c226f5c195c9b9378eb5f8412bda0c5f7df5
gw3n <loaye1978@gmail.com>
2019-09-10 23:32:45 +0200

    correctif html + PAGE_TITLE page products by categorie

--- STORE-APP/mvc_fo/app/controller/product/productbycat.php
+++ STORE-APP/mvc_fo/app/controller/product/productbycat.php
@@ -3,6 +3,6 @@
 include ("../app/model/product/display_produit.php");
 $procatal=display_produit($_GET["id"]);
 if(!empty($procatal)){
-    define("PAGE_TITLE", "Catalogue de nos chocolats par".$procatal[0]['cat_descr']." | ".SITE_NAME);
+    define("PAGE_TITLE", "Catalogue de nos chocolats ".$procatal[0]['cat_descr']." | ".SITE_NAME);
 }
 include ("../app/view/product/productsbycat.php");
